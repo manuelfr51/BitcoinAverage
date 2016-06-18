9553a013eb364cba188f06df5930bb729e2a532f
Manuel Flores <manuelflores51@hotmail.com>
2016-06-18 16:25:04 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,18 +1,18 @@
-var currency = 'USD';
+var currency = 'MXN';
 var color_up = [255, 0, 0, 255];
 var color_down = [0, 255, 0, 255];
-var api_url_base = 'https://api.bitcoinaverage.com/ticker/';
-var click_url_base = 'https://bitcoinaverage.com/#';
+var api_url_base = 'https://api.bitso.com/v2/ticker';
+var click_url_base = 'https://bitso.com/#';
 
 function updateTicker() {
   var xhr = new XMLHttpRequest();
-  xhr.open('GET', api_url_base + currency, true);
+  xhr.open('GET', api_url_base, true);
   xhr.onreadystatechange = function() {
     if (xhr.readyState == 4) {
       var ticker = JSON.parse(xhr.responseText);
       chrome.browserAction.setBadgeText({ text: '' + Math.floor(ticker['last']) });
       var bg = color_up;
-      if (ticker['last'] >= ticker['24h_avg'])
+      if (ticker['last'] >= ticker['vwap'])
         bg = color_down;
       chrome.browserAction.setBadgeBackgroundColor({ color: bg });
     }
@@ -24,7 +24,7 @@ chrome.alarms.onAlarm.addListener(function(alarm) {
   if (alarm)
     updateTicker();
 });
-chrome.alarms.create('update', { delayInMinutes: 0, periodInMinutes:1 });
+chrome.alarms.create('update', { delayInMinutes: 0, periodInMinutes:3 });
 
 updateTicker();
 
